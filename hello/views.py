f5ad97112b078e1f46aadd33340409e97b4b7dc3
28kayak <kaya.ota@gmail.com>
2018-07-05 02:20:58 +0900

    valued path

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 # Create your views here.
-def index(request):
-	#return HttpResponse("Hello Django");
-	msg = request.GET['msg']
-	return HttpResponse("You Typed: " + msg + ".")
+#def index(request):
+#	if "msg" in request.GET:
+#		msg = request.GET['msg']
+#		result = "You Typed: " + msg + "."
+#	else:
+#		result = "Parameters are missing"
+#
+#	return HttpResponse(result)
+
+def index(request, id, nickname):
+    #recieve http://localhost:8000/hello/123/kaya/
+    result = "Your ID: " + str(id) + " Name: " + str(nickname)
+    return HttpResponse(result)
+
