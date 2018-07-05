2236db7f360e237b4d645902e525848c7c2f5972
28kayak <kaya.ota@gmail.com>
2018-07-05 11:29:15 +0900

    use html as template

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	#path("", views.index, name="index"),
-	path('<int:id>/<nickname>/',views.index, name='index'),
+	#path('<int:id>/<nickname>/',views.index, name='index'),
+    path('', views.index, name='index'),    #path used by index.html in templates
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+
 
 # Create your views here.
+#URL access with parameter named msg
 #def index(request):
 #	if "msg" in request.GET:
 #		msg = request.GET['msg']
@@ -11,8 +13,11 @@ from django.http import HttpResponse
 #
 #	return HttpResponse(result)
 
-def index(request, id, nickname):
-    #recieve http://localhost:8000/hello/123/kaya/
-    result = "Your ID: " + str(id) + " Name: " + str(nickname)
-    return HttpResponse(result)
+#def index(request, id, nickname):
+#    #recieve http://localhost:8000/hello/123/kaya/
+#    result = "Your ID: " + str(id) + " Name: " + str(nickname)
+#    return HttpResponse(result)
 
+def index(request):
+    #return render(request, 'hello/index.html')
+    return render(request, 'hello_temp/index.html')
